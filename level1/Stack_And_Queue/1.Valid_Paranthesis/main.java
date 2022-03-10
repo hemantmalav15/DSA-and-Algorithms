cbfdac2b5c7da46ea21a305d0196e84fd16996bc
hemantmalav15 <malavhemant5@gmail.com>
2022-03-10 10:44:06 +0530

    added

---
+++ level1/Stack_And_Queue/1.Valid_Paranthesis/main.java
@@ -0,0 +1,30 @@
+public class main {
+
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.size() == 0) {
+                    return false;
+                } else if (ch == ')' && st.peek() != '(') {
+                    return false;
+                } else if (ch == '}' && st.peek() != '{') {
+                    return false;
+                } else if (ch == ']' && st.peek() != '[') {
+                    return false;
+                } else {
+                    st.pop();
+                }
+            }
+        }
+        if (st.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+}
